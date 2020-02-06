646a80025198b4e2f14850d2cb7502defac1087d
Oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-02-06 13:33:25 +0100

    added modification of response entity to MyResponse, getting the details of each task, getting all tasks, getting task by status

--- src/main/java/com/example/project/controller/TaskController.java
+++ src/main/java/com/example/project/controller/TaskController.java
@@ -2,14 +2,19 @@ package com.example.project.controller;
 
 import com.example.project.models.Task;
 import com.example.project.services.TaskService;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
 @RestController
 @RequestMapping("/tasks")
+
 public class TaskController {
     @Value("${server.port}")
     private String port;
@@ -24,12 +29,44 @@ public class TaskController {
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task){
+    public MyResponse<Task> createTask(@RequestBody Task task, HttpServletResponse response){
         Task t = taskService.createTask(task);
         HttpStatus statusCode = HttpStatus.CREATED;
+        String message = "Todo created successfully";
         if(t == null){
             statusCode = HttpStatus.BAD_REQUEST;
+            message = "common get out with your bad request!!!";
         }
-        return new ResponseEntity<>(t, statusCode);
+        response.setStatus(statusCode.value());
+        return new MyResponse<>(statusCode, message, t);
+    }
+    @GetMapping
+    @RequestMapping("{id}")
+    public  MyResponse<Task> getOneTask(@PathVariable Long id){
+        Task t = taskService.getATask(id);
+        if(t == null){
+            return new MyResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "No task with this id: " + id, null);
+        }
+        return  new MyResponse<>(HttpStatus.OK, "Retrieved Successfully", t);
+    }
+
+    @GetMapping
+    @RequestMapping("/all")
+    public MyResponse<List<Task>> getAllTask(){
+        List<Task> t = taskService.getAllTask();
+        if(t.isEmpty()){
+            return new MyResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "No task Available", null);
+        }
+        return  new MyResponse<>(HttpStatus.OK, "Retrieved Successfully", t);
+    }
+
+    @GetMapping
+    @RequestMapping("/Status/{status}")
+    public MyResponse<List<Task>> findBystatus(@PathVariable String status){
+        List<Task> t = taskService.findByStatus(status);
+        if(t.isEmpty()){
+            return new MyResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "No task Available", null);
+        }
+        return  new MyResponse<>(HttpStatus.OK, "Retrieved Successfully", t);
     }
 }
